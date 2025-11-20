b8dfc893f0c8bb491b2496fb98393ea50726765a
root <root@email.com>
2025-11-20 16:31:52 +0000

    Initial commit from GitAgent

--- snippets/messaging/cloud_messaging.py
+++ snippets/messaging/cloud_messaging.py
@@ -18,13 +18,10 @@ import datetime
 
 from firebase_admin import messaging
 
-
 def send_to_token():
     # [START send_to_token]
-    # This registration token comes from the client FCM SDKs.
     registration_token = 'YOUR_REGISTRATION_TOKEN'
 
-    # See documentation on defining a message payload.
     message = messaging.Message(
         data={
             'score': '850',
@@ -33,20 +30,14 @@ def send_to_token():
         token=registration_token,
     )
 
-    # Send a message to the device corresponding to the provided
-    # registration token.
     response = messaging.send(message)
-    # Response is a message ID string.
     print('Successfully sent message:', response)
     # [END send_to_token]
 
-
 def send_to_topic():
     # [START send_to_topic]
-    # The topic name can be optionally prefixed with "/topics/".
     topic = 'highScores'
 
-    # See documentation on defining a message payload.
     message = messaging.Message(
         data={
             'score': '850',
@@ -55,20 +46,14 @@ def send_to_topic():
         topic=topic,
     )
 
-    # Send a message to the devices subscribed to the provided topic.
     response = messaging.send(message)
-    # Response is a message ID string.
     print('Successfully sent message:', response)
     # [END send_to_topic]
 
-
 def send_to_condition():
     # [START send_to_condition]
-    # Define a condition which will send to devices which are subscribed
-    # to either the Google stock or the tech industry topics.
     condition = "'stock-GOOG' in topics || 'industry-tech' in topics"
 
-    # See documentation on defining a message payload.
     message = messaging.Message(
         notification=messaging.Notification(
             title='$GOOG up 1.43% on the day',
@@ -77,13 +62,9 @@ def send_to_condition():
         condition=condition,
     )
 
-    # Send a message to devices subscribed to the combination of topics
-    # specified by the provided condition.
     response = messaging.send(message)
-    # Response is a message ID string.
     print('Successfully sent message:', response)
     # [END send_to_condition]
-
 
 def send_dry_run():
     message = messaging.Message(
@@ -94,16 +75,11 @@ def send_dry_run():
         token='token',
     )
 
-    # [START send_dry_run]
-    # Send a message in the dry run mode.
     response = messaging.send(message, dry_run=True)
-    # Response is a message ID string.
     print('Dry run successful:', response)
     # [END send_dry_run]
 
-
 def android_message():
-    # [START android_message]
     message = messaging.Message(
         android=messaging.AndroidConfig(
             ttl=datetime.timedelta(seconds=3600),
@@ -117,12 +93,9 @@ def android_message():
         ),
         topic='industry-tech',
     )
-    # [END android_message]
     return message
 
-
 def apns_message():
-    # [START apns_message]
     message = messaging.Message(
         apns=messaging.APNSConfig(
             headers={'apns-priority': '10'},
@@ -133,17 +106,15 @@ def apns_message():
                         body='$GOOG gained 11.80 points to close at 835.67, up 1.43% on the day.',
                     ),
                     badge=42,
+                    custom_data={"event": "end"}  # Ensure compliance with Apple's APNs requirements
                 ),
             ),
         ),
         topic='industry-tech',
     )
-    # [END apns_message]
     return message
 
-
 def webpush_message():
-    # [START webpush_message]
     message = messaging.Message(
         webpush=messaging.WebpushConfig(
             notification=messaging.WebpushNotification(
@@ -154,12 +125,9 @@ def webpush_message():
         ),
         topic='industry-tech',
     )
-    # [END webpush_message]
     return message
 
-
 def all_platforms_message():
-    # [START multi_platforms_message]
     message = messaging.Message(
         notification=messaging.Notification(
             title='$GOOG up 1.43% on the day',
@@ -180,58 +148,35 @@ def all_platforms_message():
         ),
         topic='industry-tech',
     )
-    # [END multi_platforms_message]
     return message
-
 
 def subscribe_to_topic():
     topic = 'highScores'
-    # [START subscribe]
-    # These registration tokens come from the client FCM SDKs.
     registration_tokens = [
         'YOUR_REGISTRATION_TOKEN_1',
-        # ...
         'YOUR_REGISTRATION_TOKEN_n',
     ]
 
-    # Subscribe the devices corresponding to the registration tokens to the
-    # topic.
     response = messaging.subscribe_to_topic(registration_tokens, topic)
-    # See the TopicManagementResponse reference documentation
-    # for the contents of response.
     print(response.success_count, 'tokens were subscribed successfully')
-    # [END subscribe]
-
 
 def unsubscribe_from_topic():
     topic = 'highScores'
-    # [START unsubscribe]
-    # These registration tokens come from the client FCM SDKs.
     registration_tokens = [
         'YOUR_REGISTRATION_TOKEN_1',
-        # ...
         'YOUR_REGISTRATION_TOKEN_n',
     ]
 
-    # Unubscribe the devices corresponding to the registration tokens from the
-    # topic.
     response = messaging.unsubscribe_from_topic(registration_tokens, topic)
-    # See the TopicManagementResponse reference documentation
-    # for the contents of response.
     print(response.success_count, 'tokens were unsubscribed successfully')
-    # [END unsubscribe]
-
 
 def send_each():
     registration_token = 'YOUR_REGISTRATION_TOKEN'
-    # [START send_each]
-    # Create a list containing up to 500 messages.
     messages = [
         messaging.Message(
             notification=messaging.Notification('Price drop', '5% off all electronics'),
             token=registration_token,
         ),
-        # ...
         messaging.Message(
             notification=messaging.Notification('Price drop', '2% off all books'),
             topic='readers-club',
@@ -239,17 +184,11 @@ def send_each():
     ]
 
     response = messaging.send_each(messages)
-    # See the BatchResponse reference documentation
-    # for the contents of response.
     print(f'{response.success_count} messages were sent successfully')
-    # [END send_each]
 
 def send_each_for_multicast_and_handle_errors():
-    # [START send_each_for_multicast_error]
-    # These registration tokens come from the client FCM SDKs.
     registration_tokens = [
         'YOUR_REGISTRATION_TOKEN_1',
-        # ...
         'YOUR_REGISTRATION_TOKEN_N',
     ]
 
@@ -263,7 +202,5 @@ def send_each_for_multicast_and_handle_errors():
         failed_tokens = []
         for idx, resp in enumerate(responses):
             if not resp.success:
-                # The order of responses corresponds to the order of the registration tokens.
                 failed_tokens.append(registration_tokens[idx])
         print(f'List of tokens that caused failures: {failed_tokens}')
-    # [END send_each_for_multicast_error]
